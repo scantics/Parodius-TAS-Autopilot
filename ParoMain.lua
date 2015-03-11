14935383152a73d69fe1092e4902f2176ea2de4f
scantics <abpeters@ucsd.edu>
2015-03-11 10:45:03 -0700

    Create ParoMain.lua
    basic skeleton so I have something to work from when i get home and don't feel like *starting* a project

---
+++ ParoMain.lua
@@ -0,0 +1,24 @@
+INPUT_0  = 1
+INPUT_1  = 2
+INPUT_2  = 4
+INPUT_3  = 8
+INPUT_4  = 16
+INPUT_5  = 32
+INPUT_6  = 64
+INPUT_7  = 128
+INPUT_8  = 256
+INPUT_9  = 512
+INPUT_10 = 1024
+
+INPUT_NULL = 0
+
+meX = 0
+meY = 0
+canShoot = false
+
+--dudewat no colons this is spooky where's muh python
+function decideInput()
+    --probably not bytes, will determine the correct read when I get home and have the game in front of me
+    meX = memory.readbyte(0x000B88)
+    meY = memory.readbyte(0x000B8C)
+    
