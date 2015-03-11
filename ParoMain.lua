908225f87d620f8d4bcde67b3f76936722f4c598
scantics <abpeters@ucsd.edu>
2015-03-11 11:55:46 -0700

    very skeliten wow so spook
    for loop to read in the object table, can't fill in without more game research

--- ParoMain.lua
+++ ParoMain.lua
@@ -23,4 +23,6 @@ function decideInput()
     --probably not bytes, will determine the correct read when I get home and have the game in front of me
     meX = memory.readbyte(0x000B88)
     meY = memory.readbyte(0x000B8C)
-    
+    for i = 0x000BD8, 0x0001B88, 0x50 do
+        
+    end
