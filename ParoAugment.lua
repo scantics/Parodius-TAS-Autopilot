60361bddbecd703d97c29891619f78f801929674
scantics <abpeters@ucsd.edu>
2015-03-12 22:47:18 -0700

    should work unless there are lua problems

--- ParoAugment.lua
+++ ParoAugment.lua
@@ -1,24 +1,35 @@
 
 function map(f, arr)
     for index,value in ipairs(arr) do
-        result[index] = f(value)
+        result[index] = f(index,value)
     end
     return result
+end
         
 meX = 0
 meY = 0
 canShoot = false
 
+function boxSprite(name,position)
+    gui.drawRectangle(position[1],position[2],5,5)
+
 --dudewat no colons this is spooky where's muh python
-function decideInput()
+function highlightSprites()
     --probably not bytes, will determine the correct read when I get home and have the game in front of me
     meX = memory.readbyte(0x000B88)
     meY = memory.readbyte(0x000B8C)
-    positionTable = {}
-    for i = 0x000BD8, 0x0001B88, 0x50 do
-    
+    --seriously this language is like made of marshmallows
+    --this assignment is index-explicit for a reason
+    --because {meX,meY}[0] == nil
+    --like wow
+    positionTable["me"][0] = meX
+    positionTable["me"][1] = meY
+    --not sure how big the object table is but on frame 3737
+    for i = 0x000BD8, 0x000F9E, 0x50 do
+        positionTable[i][0] = memory.readdword[i]
+        positionTable[i][0] = memory.readdword[i+2]
     end
-    
+    map(drawRectangle(boxSprite,positionTable))
     
     
 end
